bd6e92998ab514efd12afdf8a92ea7124752f692
leolidba <leo.li.dba@gmail.com>
2016-01-20 15:15:35 -0800

    devel_init.sh
    devel init

---
+++ init.sh
@@ -0,0 +1,27 @@
+
+#-Oracle's JDK
+
+yum install centos-release-scl.noarch
+yum groupinstall "Development Tools" # yum groupinstall development
+
+#AND
+
+yum install -y ant
+yum install -y asciidoc
+yum install -y cyrus-sasl-devel
+yum install -y cyrus-sasl-gssapi
+yum install -y gcc
+yum install -y gcc-c++
+yum install -y krb5-devel
+yum install -y libtidy #(for unit tests only)
+yum install -y libxml2-devel
+yum install -y libxslt-devel
+yum install -y make
+yum install -y mvn #(from apache-maven package or maven3 tarball)
+yum install -y mysql
+yum install -y mysql-devel
+yum install -y openldap-devel
+yum install -y python-devel
+yum install -y sqlite-devel
+yum install -y openssl-devel #(for version 7+)
+yum install -y gmp-devel
