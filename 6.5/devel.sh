184141a024075b8d49e15db0189aa78df3dad7e0
leolidba <leo.li.dba@gmail.com>
2016-02-06 08:22:47 -0800

    Create devel.sh

---
+++ 6.5/devel.sh
@@ -0,0 +1,11 @@
+yum install rpm
+which scp curl wget unzip chkconfig tar
+yum repolist
+yum install yum-utils createrepo reposync
+
+yum install  ant asciidoc cyrus-sasl-devel cyrus-sasl-gssapi gcc gcc-c++ krb5-devel 
+yum install  libtidy libxml2-devel libxslt-devel make 
+yum install  mysql mysql-devel 
+yum install  openldap-devel python-devel sqlite-devel openssl-devel  gmp-devel
+
+-- Install MVN
