5ee5a8db1b36d8f1a1000835512101b8aaa0f71a
leolidba <leo.li.dba@gmail.com>
2016-02-06 08:46:33 -0800

    install java openjdk 1.7.0 and set JAVA_HOME

--- 6.5/devel.sh
+++ 6.5/devel.sh
@@ -8,4 +8,15 @@ yum install  libtidy libxml2-devel libxslt-devel make
 yum install  mysql mysql-devel 
 yum install  openldap-devel python-devel sqlite-devel openssl-devel  gmp-devel
 
+
+yum install java-1.7.0-openjdk-devel.x86_64
+
+echo '#!/bin/bash
+JAVA_HOME=/etc/alternatives/java_sdk_openjdk
+PATH=$JAVA_HOME/bin:$PATH
+export PATH JAVA_HOME
+export CLASSPATH=.
+' > /etc/profile.d/java.sh 
+
+
 -- Install MVN
