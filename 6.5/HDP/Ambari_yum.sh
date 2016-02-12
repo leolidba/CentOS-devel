7f16d523b8dca856ecc291a601ddd21e1d946ddf
leolidba <leo.li.dba@gmail.com>
2016-02-11 23:59:39 -0800

    jdk update

--- 6.5/HDP/Ambari_yum.sh
+++ 6.5/HDP/Ambari_yum.sh
@@ -18,6 +18,10 @@ ambari-server setup
 ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar
 # File copied to /var/lib/ambari-server/resources
 
+# Use an alternative new jdk
+ambari-server setup -j /etc/alternatives/java_sdk_1.8.0
+
+
 ambari-server start
 echo browse via http://`hostname`:8080
 
