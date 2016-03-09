593333fe0b38d855f5550ba78e0b88bb79e34aa8
leolidba <leo.li.dba@gmail.com>
2016-03-09 13:23:14 -0800

    Update setup.sh

--- 6.5/HBase/setup.sh
+++ 6.5/HBase/setup.sh
@@ -9,4 +9,8 @@
 #Reference: 
 #1. https://docs.hortonworks.com/HDPDocuments/HDP2/HDP-2.3.0/bk_installing_manually_book/content/ref-2a6efe32-d0e1-4e84-9068-4361b8c36dc8.1.html
 
-nohup /usr/bin/hbase thrift start > /var/log/hbase-thrift-start.log 2>& 1 &
+
+# Start thrift 
+# opt 1:     nohup /usr/bin/hbase thrift start > /var/log/hbase-thrift-start.log 2>& 1 &
+# opt 2:     /usr/hdp/current/hbase-client/bin/hbase-daemon.sh start thrift
+#             starting thrift, logging to /var/log/hbase/hbase-root-thrift-<servename>.out
